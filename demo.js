1988062705107776bee75e3735a2f3e89b803fbf
Nitin Kumar Dey <72318174+nitinkumar333@users.noreply.github.com>
2021-06-21 13:05:12 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,7 @@
+var name = prompt("Enter your name");
+var firstLetter = name.slice(0,1);
+var uppercase1 = firstLetter.toUpperCase();
+var restLetters = name.slice(1,name.length);
+var lowercase1 = restLetters.toLowerCase();
+var finalName = uppercase1 + lowercase1;
+alert("Hello, " +finalName+ " Nice to see you");
