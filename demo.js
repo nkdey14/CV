b0627af97f82d5775b354761f7514319cc827fdc
Nitin Kumar Dey <72318174+nitinkumar333@users.noreply.github.com>
2021-06-21 13:23:22 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -4,4 +4,4 @@ var uppercase1 = firstLetter.toUpperCase();
 var restLetters = name.slice(1,name.length);
 var lowercase1 = restLetters.toLowerCase();
 var finalName = uppercase1 + lowercase1;
-alert("Hello, " +finalName+ " Nice to see you");
+alert("Hello, " +finalName+ "!" + " Nice to see you");
